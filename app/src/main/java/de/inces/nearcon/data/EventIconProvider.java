be6363195d0b2de867ab4c83b58c3db2e9b457cd
lukasbecker <lukas.becker@onlinehome.de>
2017-05-31 01:53:58 +0200

    more icons

--- app/src/main/java/de/inces/nearcon/data/EventIconProvider.java
+++ app/src/main/java/de/inces/nearcon/data/EventIconProvider.java
@@ -17,8 +17,21 @@ public class EventIconProvider {
         icons.add(new EventIcon(1, "running"));
         icons.add(new EventIcon(1, "soccer"));
         icons.add(new EventIcon(1, "swimming"));
+        icons.add(new EventIcon(1, "tennis"));
         icons.add(new EventIcon(2, "beer"));
         icons.add(new EventIcon(2, "cocktail"));
+        icons.add(new EventIcon(2, "wine"));
+        icons.add(new EventIcon(2, "dancing"));
+        icons.add(new EventIcon(2, "partyhat"));
+        icons.add(new EventIcon(3, "cooking"));
+        icons.add(new EventIcon(3, "mensa"));
+        icons.add(new EventIcon(3, "pizza"));
+        icons.add(new EventIcon(3, "tea"));
+        icons.add(new EventIcon(3, "movie"));
+        icons.add(new EventIcon(3, "heart"));
+        icons.add(new EventIcon(3, "sunbathe"));
+        icons.add(new EventIcon(3, "zoo"));
+        icons.add(new EventIcon(3, "games"));
     }
 
     public List<EventIcon> getIconsByCategory(int categoryId) {
