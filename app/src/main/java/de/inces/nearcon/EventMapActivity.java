913175b8ed9abf120e39cc9642fdf80c9f813264
plegit <philipplegran@gmail.com>
2017-05-30 18:51:49 +0200

    fab button added

--- app/src/main/java/de/inces/nearcon/EventMapActivity.java
+++ app/src/main/java/de/inces/nearcon/EventMapActivity.java
@@ -1,10 +1,17 @@
 package de.inces.nearcon;
 
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,10 +29,40 @@ public class EventMapActivity extends AppCompatActivity implements EventPageFrag
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         EventMapFragment mapFragment = (EventMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.frag_map);
+        FloatingActionButton newEventButton = (FloatingActionButton) findViewById(R.id.fab_newEvent);
+        newEventButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
+                showPopup(v);
+            }
+        });;
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    public void showPopup(View anchorView) {
+
+        View popupView = getLayoutInflater().inflate(R.layout.popup_layout, null);
+
+        PopupWindow popupWindow = new PopupWindow(popupView,
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);;
+
+        // If the PopupWindow should be focusable
+        popupWindow.setFocusable(true);
+
+        // If you need the PopupWindow to dismiss when when touched outside
+        popupWindow.setBackgroundDrawable(new ColorDrawable());
+
+        int location[] = new int[2];
+
+        // Get the View's(the one that was clicked in the Fragment) location
+        anchorView.getLocationOnScreen(location);
+
+        // Using location, the PopupWindow will be displayed right under anchorView
+        popupWindow.showAsDropDown(anchorView);
     }
 }
--- core/src/main/java/de/inces/nearcon/events/Event.java
+++ core/src/main/java/de/inces/nearcon/events/Event.java
@@ -19,4 +19,31 @@ public class Event {
 
     private EventVisibility Visibility;
 
+    public User getCreator() {
+        return Creator;
+    }
+
+    public EventCategory getCategory() {
+        return Category;
+    }
+
+    public EventIcon getIcon() {
+        return Icon;
+    }
+
+    public Calendar getExpireTime() {
+        return ExpireTime;
+    }
+
+    public EventLocation getLocation() {
+        return Location;
+    }
+
+    public int getExclusivity() {
+        return Exclusivity;
+    }
+
+    public EventVisibility getVisibility() {
+        return Visibility;
+    }
 }
