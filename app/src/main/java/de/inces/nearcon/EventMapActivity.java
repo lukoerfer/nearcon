81554641a51040f90fc8f52d4bb1b7b009801014
plegit <philipplegran@gmail.com>
2017-05-30 15:35:59 +0200

    Initial fragment implementation of map

---
+++ app/src/main/java/de/inces/nearcon/EventMapActivity.java
@@ -0,0 +1,31 @@
+package de.inces.nearcon;
+
+import android.net.Uri;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class EventMapActivity extends AppCompatActivity implements EventPageFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        EventMapFragment mapFragment = (EventMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.frag_map);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
---
+++ app/src/main/java/de/inces/nearcon/EventMapFragment.java
@@ -0,0 +1,35 @@
+package de.inces.nearcon;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class EventMapFragment extends SupportMapFragment implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle bundle){
+        this.getMapAsync(this);
+        return super.onCreateView(inflater,group,bundle);
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
